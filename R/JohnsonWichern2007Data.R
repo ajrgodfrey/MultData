236ba98a93bead1ef37d1fb2f28edae02287bfdd
Jonathan Godfrey <a.j.godfrey@massey.ac.nz>
2023-08-20 15:28:15 +1200

    adding JW07 help doc

---
+++ R/JohnsonWichern2007Data.R
@@ -0,0 +1,11 @@
+#' Data from Johnson and Wichern (2007)
+#'
+#' Tables from Johnson and Wichern (2007) have been imported from csv files. These were created from tab delimited text files.
+#'
+#' @name JW2007
+#' @rdname JW2007
+#' @details The data has undergone minimal cleaning.
+#' @references Johnson, and Wichern, (2007) \emph{Applied Multivariate Statistical Analysis}, sixth edition, 
+#' @author contributed by Jonathan Godfrey
+NULL
+
